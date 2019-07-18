005c16f5a320e544938051dfdc74d7b50d5f9fd8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-18 17:25:19 +0000

    Deploy city-of-kingston/mayors-innovation-challenge-2020 to github.com/city-of-kingston/mayors-innovation-challenge-2020.git:gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# mayors-innovation-challenge-2020
-Microsite for the Mayor's Innovation Challenge 2020 - Built using React and Next.js 😎
--- components/content/index.js
+++
@@ -1,6 +0,0 @@
-import Content from './Content';
-import ContentDiv from './ContentDiv';
-import ContentParagraph from './ContentParagraph';
-
-export default Content;
-export { ContentDiv, ContentParagraph };
