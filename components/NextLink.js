b0e8baf118c8f5862b5a1749e7f0232edd2adc6a
Ryan Rossiter <nayr0788@gmail.com>
2019-08-23 09:41:03 -0400

    Fix links after domain change

--- components/NextLink.js
+++ components/NextLink.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-const urlPrefix = process.env.NODE_ENV === 'production' ? '/' : '';
+const urlPrefix = process.env.NODE_ENV === 'production' ? '' : '';
 
 const NextLink = ({ href, children }) => (
 	<Link href={urlPrefix + href}>{children}</Link>
